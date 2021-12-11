095086b33552c323351111f6c4bb30f9ac7d88d6
Adam Zambrzycki <adam@zambrzycki.net>
2021-12-11 01:27:14 +0100

    Add missing newlines

--- extractor/fetcher.go
+++ extractor/fetcher.go
@@ -71,14 +71,14 @@ func FetchFromLdap(address *url.URL) ([]string, error) {
 		if class == "javaNamingReference" {
 			filename, err := DownloadPayload(entry)
 			if err != nil {
-				fmt.Printf("Failed to download payload: %v", err)
+				fmt.Printf("Failed to download payload: %v\n", err)
 				continue
 			}
 			files = append(files, filename)
 		} else {
 			filename, err := SaveDetails(entry)
 			if err != nil {
-				fmt.Printf("Failed to save payload: %v", err)
+				fmt.Printf("Failed to save payload: %v\n", err)
 				continue
 			}
 			files = append(files, filename)
