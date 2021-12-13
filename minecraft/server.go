fb81cffa7eaa7505877a6b35ed3b1c54f0ca63f7
Adam Zambrzycki <adam@zambrzycki.net>
2021-12-13 12:48:34 +0100

    Add support from 1.7.2 to 1.16.5

--- minecraft/server.go
+++ minecraft/server.go
@@ -10,17 +10,20 @@ import (
 	"github.com/Tnze/go-mc/offline"
 	"github.com/google/uuid"
 	"log"
+	"strings"
 )
 
 const (
-	PlayerPositionAndLookClientbound = 0x34
-	JoinGame                         = 0x24
-	ProtocolVersion                  = 754
-	MaxPlayer                        = 50
+	MaxPlayer = 50
 )
 
+type Session struct {
+	Server          *Server
+	ProtocolVersion int32
+}
+
 type Server struct {
-	Address string
+	Address             string
 	AcceptLoginCallback func(userName string)
 	ChatMessageCallback func(text string)
 }
@@ -67,17 +70,22 @@ func (s *Server) acceptConn(conn net.Conn) {
 		return
 	}
 
+	session := Session{
+		Server:          s,
+		ProtocolVersion: protocol,
+	}
+
 	switch intention {
 	default: //unknown error
 		log.Printf("Unknown handshake intention: %v", intention)
 	case 1: //for status
-		s.acceptListPing(conn)
+		session.acceptListPing(conn)
 	case 2: //for login
-		s.handlePlaying(conn, protocol)
+		session.handlePlaying(conn, protocol)
 	}
 }
 
-func (s *Server) handlePlaying(conn net.Conn, protocol int32) {
+func (s *Session) handlePlaying(conn net.Conn, protocol int32) {
 	// login, get player info
 	info, err := s.acceptLogin(conn)
 	if err != nil {
@@ -86,7 +94,6 @@ func (s *Server) handlePlaying(conn net.Conn, protocol int32) {
 	}
 
 	// Write LoginSuccess packet
-
 	if err = s.loginSuccess(conn, info.Name, info.UUID); err != nil {
 		log.Print("Login failed on success")
 		return
@@ -96,12 +103,8 @@ func (s *Server) handlePlaying(conn net.Conn, protocol int32) {
 		log.Print("Login failed on joinGame")
 		return
 	}
-	if err := conn.WritePacket(pk.Marshal(PlayerPositionAndLookClientbound,
-		pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
-		pk.Float(0), pk.Float(0), // Yaw Pitch
-		pk.Byte(0),        // flag
-		pk.VarInt(0),      // TP ID
-	)); err != nil {
+
+	if err := s.playerPositionAndLookClientbound(conn); err != nil {
 		log.Printf("Login failed on sending PlayerPositionAndLookClientbound: %v", err)
 		return
 	}
@@ -116,14 +119,29 @@ func (s *Server) handlePlaying(conn net.Conn, protocol int32) {
 			break
 		}
 
-		if p.ID == packetid.ChatServerbound {
+		var chatPacketId int32
+		if s.ProtocolVersion == 754 {
+			chatPacketId = packetid.ChatServerbound
+		} else if s.ProtocolVersion >= 107 && s.ProtocolVersion <= 316 {
+			chatPacketId = 0x02
+		} else if s.ProtocolVersion == 335 {
+			chatPacketId = 0x03
+		} else if s.ProtocolVersion >= 338 && s.ProtocolVersion <= 404 {
+			chatPacketId = 0x02
+		} else if s.ProtocolVersion >= 477 {
+			chatPacketId = 0x03
+		} else {
+			chatPacketId = 0x01
+		}
+
+		if p.ID == chatPacketId {
 			var message pk.String
 			if err := p.Scan(&message); err != nil {
 				continue
 			}
 
-			if s.ChatMessageCallback != nil {
-				s.ChatMessageCallback(string(message))
+			if s.Server.ChatMessageCallback != nil {
+				s.Server.ChatMessageCallback(string(message))
 			}
 		}
 		// KeepAlive packet is not handled, so client might
@@ -138,7 +156,7 @@ type PlayerInfo struct {
 }
 
 // acceptLogin check player's account
-func (s *Server) acceptLogin(conn net.Conn) (info PlayerInfo, err error) {
+func (s *Session) acceptLogin(conn net.Conn) (info PlayerInfo, err error) {
 	//login start
 	var p pk.Packet
 	err = conn.ReadPacket(&p)
@@ -153,8 +171,8 @@ func (s *Server) acceptLogin(conn net.Conn) (info PlayerInfo, err error) {
 
 	info.UUID = offline.NameToUUID(info.Name)
 
-	if s.AcceptLoginCallback != nil {
-		s.AcceptLoginCallback(info.Name)
+	if s.Server.AcceptLoginCallback != nil {
+		s.Server.AcceptLoginCallback(info.Name)
 	}
 	return
 }
@@ -179,36 +197,208 @@ func (s *Server) handshake(conn net.Conn) (protocol, intention int32, err error)
 }
 
 // loginSuccess send LoginSuccess packet to client
-func (s *Server) loginSuccess(conn net.Conn, name string, uuid uuid.UUID) error {
-	return conn.WritePacket(pk.Marshal(0x02,
-		pk.UUID(uuid),
-		pk.String(name),
-	))
+func (s *Session) loginSuccess(conn net.Conn, name string, uuid uuid.UUID) error {
+	if s.ProtocolVersion <= 4 {
+		return conn.WritePacket(pk.Marshal(0x02,
+			pk.String(strings.ReplaceAll(uuid.String(), "-", "")),
+			pk.String(name),
+		))
+	} else if s.ProtocolVersion <= 578 {
+		return conn.WritePacket(pk.Marshal(0x02,
+			pk.String(uuid.String()),
+			pk.String(name),
+		))
+	} else {
+		return conn.WritePacket(pk.Marshal(0x02,
+			pk.UUID(uuid),
+			pk.String(name),
+		))
+	}
 }
 
 //go:embed DimensionCodec.snbt
 var dimensionCodecSNBT string
 
+//go:embed DimensionCodec2.snbt
+var dimensionCodec2SNBT string
+
 //go:embed Dimension.snbt
 var dimensionSNBT string
 
-func (s *Server) joinGame(conn net.Conn) error {
-	return conn.WritePacket(pk.Marshal(JoinGame,
-		pk.Int(0),          // EntityID
-		pk.Boolean(false),  // Is hardcore
-		pk.UnsignedByte(1), // Gamemode
-		pk.Byte(1),         // Previous Gamemode
-		pk.VarInt(1),       // World Count
-		pk.Ary{Len: 1, Ary: []pk.Identifier{"world"}},      // World Names
-		pk.NBT(nbt.StringifiedMessage(dimensionCodecSNBT)), // Dimension codec
-		pk.NBT(nbt.StringifiedMessage(dimensionSNBT)),      // Dimension
-		pk.Identifier("world"),                             // World Name
-		pk.Long(0),                                         // Hashed Seed
-		pk.VarInt(MaxPlayer),                               // Max Players
-		pk.VarInt(15),                                      // View Distance
-		pk.Boolean(false),                                  // Reduced Debug Info
-		pk.Boolean(true),                                   // Enable respawn screen
-		pk.Boolean(false),                                  // Is Debug
-		pk.Boolean(true),                                   // Is Flat
-	))
+func (s *Session) joinGame(conn net.Conn) error {
+	if s.ProtocolVersion <= 47 && s.ProtocolVersion > 5 {
+		return conn.WritePacket(pk.Marshal(0x01,
+			pk.Int(0),          // EntityID
+			pk.UnsignedByte(1), // Gamemode
+			pk.Byte(0),
+			pk.UnsignedByte(0),
+			pk.UnsignedByte(MaxPlayer),
+			pk.String("default"),
+			pk.Boolean(true),
+		))
+	} else if s.ProtocolVersion == 107 {
+		return conn.WritePacket(pk.Marshal(0x23,
+			pk.Int(0),          // EntityID
+			pk.UnsignedByte(1), // Gamemode
+			pk.Byte(0),
+			pk.UnsignedByte(0),
+			pk.UnsignedByte(MaxPlayer),
+			pk.String("default"),
+			pk.Boolean(true),
+		))
+	} else if s.ProtocolVersion >= 108 && s.ProtocolVersion <= 340 {
+		return conn.WritePacket(pk.Marshal(0x23,
+			pk.Int(0),          // EntityID
+			pk.UnsignedByte(1), // Gamemode
+			pk.Int(0), // changed
+			pk.UnsignedByte(0),
+			pk.UnsignedByte(MaxPlayer),
+			pk.String("default"),
+			pk.Boolean(true),
+		))
+	} else if s.ProtocolVersion >= 393 && s.ProtocolVersion <= 404 {
+		return conn.WritePacket(pk.Marshal(0x25,
+			pk.Int(0),          // EntityID
+			pk.UnsignedByte(1), // Gamemode
+			pk.Int(0), // changed
+			pk.UnsignedByte(0),
+			pk.UnsignedByte(MaxPlayer),
+			pk.String("default"),
+			pk.Boolean(true),
+		))
+	} else if s.ProtocolVersion >= 477 && s.ProtocolVersion <= 498 {
+		return conn.WritePacket(pk.Marshal(0x25,
+			pk.Int(0),          // EntityID
+			pk.UnsignedByte(1), // Gamemode
+			pk.Int(0),
+			pk.UnsignedByte(MaxPlayer),
+			pk.String("default"),
+			pk.VarInt(15),
+			pk.Boolean(true),
+		))
+	} else if s.ProtocolVersion >= 573 && s.ProtocolVersion <= 578 {
+		return conn.WritePacket(pk.Marshal(0x26,
+			pk.Int(0),          // EntityID
+			pk.UnsignedByte(1), // Gamemode
+			pk.Int(0),
+			pk.Long(0),
+			pk.UnsignedByte(MaxPlayer),
+			pk.String("default"),
+			pk.VarInt(15),
+			pk.Boolean(true),
+			pk.Boolean(true),
+		))
+	} else if s.ProtocolVersion >= 735 && s.ProtocolVersion <= 736 {
+		return conn.WritePacket(pk.Marshal(0x25,
+			pk.Int(0),                                          // EntityID
+			pk.UnsignedByte(1),                                 // Gamemode
+			pk.UnsignedByte(1),                                 // Previous Gamemode
+			pk.VarInt(1),                                       // World Count
+			pk.Ary{Len: 1, Ary: []pk.Identifier{"world"}},      // World Names
+			pk.NBT(nbt.StringifiedMessage(dimensionCodec2SNBT)), // Dimension codec
+			pk.Identifier("overworld"),
+			pk.Identifier("world"),                             // World Name
+			pk.Long(0),                                         // Hashed Seed
+			pk.VarInt(MaxPlayer),                               // Max Players
+			pk.VarInt(15),                                      // View Distance
+			pk.Boolean(false),                                  // Reduced Debug Info
+			pk.Boolean(true),                                   // Enable respawn screen
+			pk.Boolean(false),                                  // Is Debug
+			pk.Boolean(true),                                   // Is Flat
+		))
+	} else if s.ProtocolVersion >= 751 {
+		return conn.WritePacket(pk.Marshal(0x24,
+			pk.Int(0),                                          // EntityID
+			pk.Boolean(false),                                  // Is hardcore
+			pk.UnsignedByte(1),                                 // Gamemode
+			pk.Byte(1),                                         // Previous Gamemode
+			pk.VarInt(1),                                       // World Count
+			pk.Ary{Len: 1, Ary: []pk.Identifier{"world"}},      // World Names
+			pk.NBT(nbt.StringifiedMessage(dimensionCodecSNBT)), // Dimension codec
+			pk.NBT(nbt.StringifiedMessage(dimensionSNBT)),      // Dimension
+			pk.Identifier("world"),                             // World Name
+			pk.Long(0),                                         // Hashed Seed
+			pk.VarInt(MaxPlayer),                               // Max Players
+			pk.VarInt(15),                                      // View Distance
+			pk.Boolean(false),                                  // Reduced Debug Info
+			pk.Boolean(true),                                   // Enable respawn screen
+			pk.Boolean(false),                                  // Is Debug
+			pk.Boolean(true),                                   // Is Flat
+		))
+	} else {
+		return conn.WritePacket(pk.Marshal(0x01,
+			pk.Int(0),          // EntityID
+			pk.UnsignedByte(1), // Gamemode
+			pk.Byte(0),
+			pk.UnsignedByte(0),
+			pk.UnsignedByte(MaxPlayer),
+			pk.String("default"),
+		))
+	}
+}
+
+func (s *Session) playerPositionAndLookClientbound(conn net.Conn) error {
+	if s.ProtocolVersion == 754 {
+		return conn.WritePacket(pk.Marshal(0x34,
+			pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
+			pk.Float(0), pk.Float(0), // Yaw Pitch
+			pk.Byte(0),   // flag
+			pk.VarInt(0), // TP ID
+		))
+	} else if s.ProtocolVersion >= 107 && s.ProtocolVersion <= 335 {
+		return conn.WritePacket(pk.Marshal(0x2e,
+			pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
+			pk.Float(0), pk.Float(0), // Yaw Pitch
+			pk.Byte(0),   // flag
+			pk.VarInt(0), // TP ID
+		))
+	} else if s.ProtocolVersion >= 338 && s.ProtocolVersion <= 340 {
+		return conn.WritePacket(pk.Marshal(0x2f,
+			pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
+			pk.Float(0), pk.Float(0), // Yaw Pitch
+			pk.Byte(0),   // flag
+			pk.VarInt(0), // TP ID
+		))
+	} else if s.ProtocolVersion >= 393 && s.ProtocolVersion <= 404 {
+		return conn.WritePacket(pk.Marshal(0x32,
+			pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
+			pk.Float(0), pk.Float(0), // Yaw Pitch
+			pk.Byte(0),   // flag
+			pk.VarInt(0), // TP ID
+		))
+	} else if s.ProtocolVersion >= 477 && s.ProtocolVersion <= 498 {
+		return conn.WritePacket(pk.Marshal(0x35,
+			pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
+			pk.Float(0), pk.Float(0), // Yaw Pitch
+			pk.Byte(0),   // flag
+			pk.VarInt(0), // TP ID
+		))
+	} else if s.ProtocolVersion >= 573 && s.ProtocolVersion <= 578 {
+		return conn.WritePacket(pk.Marshal(0x36,
+			pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
+			pk.Float(0), pk.Float(0), // Yaw Pitch
+			pk.Byte(0),   // flag
+			pk.VarInt(0), // TP ID
+		))
+	} else if s.ProtocolVersion == 735 {
+		return conn.WritePacket(pk.Marshal(0x35,
+			pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
+			pk.Float(0), pk.Float(0), // Yaw Pitch
+			pk.Byte(0),   // flag
+			pk.VarInt(0), // TP ID
+		))
+	} else if s.ProtocolVersion >= 736 {
+		return conn.WritePacket(pk.Marshal(0x34,
+			pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
+			pk.Float(0), pk.Float(0), // Yaw Pitch
+			pk.Byte(0),   // flag
+			pk.VarInt(0), // TP ID
+		))
+	} else {
+		return conn.WritePacket(pk.Marshal(0x08,
+			pk.Double(0), pk.Double(0), pk.Double(0), // XYZ
+			pk.Float(0), pk.Float(0), // Yaw Pitch
+			pk.Boolean(false), // flag
+		))
+	}
 }
--- minecraft/status.go
+++ minecraft/status.go
@@ -10,7 +10,7 @@ import (
 	"github.com/google/uuid"
 )
 
-func (s *Server) acceptListPing(conn net.Conn) {
+func (s *Session) acceptListPing(conn net.Conn) {
 	var p pk.Packet
 	for i := 0; i < 2; i++ { // ping or list. Only accept twice
 		err := conn.ReadPacket(&p)
@@ -20,7 +20,7 @@ func (s *Server) acceptListPing(conn net.Conn) {
 
 		switch p.ID {
 		case 0x00: //List
-			err = conn.WritePacket(pk.Marshal(0x00, pk.String(listResp())))
+			err = conn.WritePacket(pk.Marshal(0x00, pk.String(s.listResp())))
 		case 0x01: //Ping
 			err = conn.WritePacket(p)
 		}
@@ -36,7 +36,7 @@ type player struct {
 }
 
 // listResp return server status as JSON string
-func listResp() string {
+func (s *Session) listResp() string {
 	var list struct {
 		Version struct {
 			Name     string `json:"name"`
@@ -51,8 +51,18 @@ func listResp() string {
 		FavIcon     string       `json:"favicon,omitempty"`
 	}
 
-	list.Version.Name = "1.16.5"
-	list.Version.Protocol = ProtocolVersion
+	list.Version.Name = "1.7.5"
+	list.Version.Protocol = int(s.ProtocolVersion)
+
+	version := s.GetVersionName()
+	if version == "" {
+		list.Version.Name = "1.16.5"
+		list.Version.Protocol = 754
+	} else {
+		list.Version.Name = version
+		list.Version.Protocol = int(s.ProtocolVersion)
+	}
+
 	list.Players.Max = MaxPlayer
 	list.Players.Online = 0
 	list.Players.Sample = []player{}
@@ -63,4 +73,44 @@ func listResp() string {
 		log.Panic("Marshal JSON for status checking fail")
 	}
 	return string(data)
+}
+
+func (s *Session) GetVersionName() string {
+	mapping := map[int32]string{
+		4: "1.7.5",
+		5: "1.7.10",
+		47: "1.8.9",
+		107: "1.9",
+		108: "1.9.1",
+		109: "1.9.2",
+		110: "1.9.4",
+		210: "1.10.2",
+		315: "1.11",
+		316: "1.11.2",
+		335: "1.12",
+		338: "1.12.1",
+		340: "1.12.2",
+		393: "1.13",
+		401: "1.13.1",
+		404: "1.13.2",
+		477: "1.14",
+		480: "1.14.1",
+		485: "1.14.2",
+		490: "1.14.3",
+		498: "1.14.4",
+		573: "1.15",
+		575: "1.15.1",
+		578: "1.15.2",
+		735: "1.16",
+		736: "1.16.1",
+		751: "1.16.2",
+		753: "1.16.3",
+		754: "1.16.5",
+	}
+
+	if _, has := mapping[s.ProtocolVersion]; !has {
+		return ""
+	}
+
+	return mapping[s.ProtocolVersion]
 }
